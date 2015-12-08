0c9375557ab3310eebf574d5f50153e6dd241ff3
Adam Rall <arall@buffalo.edu>
2015-12-08 15:22:41 -0500

    Convert to GPLv3.  Add license headers to code.

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
+# setup.py
+# Copyright (C) 2015 Adam R. Rall <arall@buffalo.edu>
+#
+# This file is part of coex.
+#
+# coex is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# coex is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with coex.  If not, see <http://www.gnu.org/licenses/>.
+
 """Installation script for coex using setuptools."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
@@ -10,6 +28,6 @@ setup(
     install_requires=['numpy', 'scipy'],
     author='Adam Rall',
     author_email='arall@buffalo.edu',
-    license='MIT',
+    license='GPLv3',
     keywords='molecular simulation',
 )
