7c6f65e5053550c6682b2cc9656e90514be39379
Adam Rall <rall41817@gmail.com>
2016-04-18 13:53:06 -0400

    Fix implementation errors in smooth.py.
    In particular, my method for smoothing lnpi_tr was wrong, and I
    neglected to consider the diagonal layout of pacc_*_cr.dat, i.e.,
    reverse attempts for subensemble i are in the left column of the
    (i+1)th row.

--- coex/smooth.py
+++ coex/smooth.py
@@ -5,13 +5,14 @@ import os.path
 import numpy as np
 
 
-def find_poorly_sampled_subensembles(transitions, cutoff):
-    """Determine which subensembles are not adequately sampled.
+def find_poorly_sampled(transitions, cutoff):
+    """Determine which subensemble/molecule/growth stage combinations
+    are not adequately sampled.
 
-    For each subensemble, we take the minimum of the number of
+    For each combination, we take the minimum of the number of
     forward and backward transitions. If this number is less than the
-    average over all subensembles times some cutoff fraction, then we
-    add it to the list of poorly sampled subensembles.
+    average over all combinations times some cutoff fraction, then we
+    add it to the list of poorly sampled combinations.
 
     Args:
         transitions: A 2D numpy array with the count of forward and
@@ -20,14 +21,14 @@ def find_poorly_sampled_subensembles(transitions, cutoff):
             sampling quality.
 
     Returns:
-        A list of subensembles which don't meet the sampling quality
+        A list of indices which don't meet the sampling quality
         threshold.
     """
-    avg = np.mean(np.amin(transitions, axis=1))
+    avg = [np.mean(min(a[1], transitions[i + 1, 0]))
+           for i, a in enumerate(transitions[:-1])]
 
-    # Skip the first subensemble.
-    return [i for i, a in enumerate(transitions[1:])
-            if np.amin(a) < cutoff * avg]
+    return [i for i, a in enumerate(transitions[:-1])
+            if min(a[1], transitions[i + 1, 0]) < cutoff * avg]
 
 
 def smooth_op(path, order, cutoff):
@@ -50,7 +51,7 @@ def smooth_op(path, order, cutoff):
     transitions = np.loadtxt(os.path.join(os.path.dirname(path), 
                                           'pacc_op_cr.dat'),
                              usecols=(1, 2))
-    drop = find_poorly_sampled_subensembles(transitions, cutoff)
+    drop = find_poorly_sampled(transitions, cutoff)
     diff = np.delete(np.diff(lnpi), drop)
     diff_idx = np.delete(index, drop)
     p = np.poly1d(np.polyfit(diff_idx, diff, order))
@@ -77,30 +78,36 @@ def smooth_tr(path, order, cutoff):
     """
     index, sub, mol, stage, lnpi = np.loadtxt(path, usecols=(0, 1, 2, 3, 4),
                                               unpack=True)
-    transitions = np.loadtxt(os.path.join(os.path.dirname(path)),
+    transitions = np.loadtxt(os.path.join(os.path.dirname(path),
                                           'pacc_tr_cr.dat'),
                              usecols=(4, 5))
-    for i in np.unique(sub):
-        current_sub = np.where(sub == i)
-        lnpi_tr[current_sub] -= lnpi_tr[current_sub][-1]
-
+    drop = find_poorly_sampled(transitions, cutoff)
+    diff = np.diff(lnpi)
     new_lnpi = np.zeros(len(lnpi))
+    fitted_diff = np.zeros(len(lnpi))
     for m in np.unique(mol):
         current_mol = np.where(mol == m)
-        lnpi_mol = lnpi[current_mol]
-        trans_mol = transitions[current_mol]
+        diff_mol = diff[current_mol]
+        fit_mol = fitted_diff[current_mol]
+        index_mol = index[current_mol]
         stage_mol = stage[current_mol]
-        for s in np.unique(stage)[:-1]:
-            current_stage = np.where(stage_mol == s)
-            drop = find_poorly_sampled_subensembles(trans_mol[current_stage],
-                                                    cutoff)
-            diff = np.delete(np.diff(lnpi_mol[current_stage]), drop)
-            p = np.poly1d(np.polyfit(range(len(diff)), diff, order))
-            x = np.unique(sub([:-1]))
-            lnpi_mol[current_stage] = (lnpi_mol[current_stage][0] +
-                                       np.append(0.0, np.cumsum(p(x))))
+        for i in np.unique(stage_mol)[:-1]:
+            current_stage = np.where(stage_mol == i)
+            diff_stg = diff_mol[current_stage]
+            index_stg = index_mol[current_stage] - 1
+            y = np.delete(diff_stg, np.where(index_stg == drop))
+            x = range(len(diff_stg))
+            p = np.poly1d(np.polyfit(x, y, order))
+            fit_mol[current_stage] = p(x)
 
-        new_lnpi[current_mol] = lnpi_mol
+        lnpi_mol = lnpi[current_mol]
+        sub_mol = sub[current_mol]
+        for s in np.unique(sub_mol):
+            current_sub = np.where(sub_mol == s)
+            diff_sub = fit_mol[current_sub]
+            lnpi_mol[current_sub] = -np.cumsum(diff_sub[::-1])[::-1]
+
+        new_lnpi[current_mol] = lnpi_mol[current_sub]
 
     return index, sub, mol, stage, new_lnpi
 
