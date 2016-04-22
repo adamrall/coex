511d2a0edf09a7edbaa8439060d97c10c3e50032
Adam Rall <rall41817@gmail.com>
2016-04-22 12:41:30 -0400

    Add docstrings to combine.py.

--- coex/combine.py
+++ coex/combine.py
@@ -39,12 +39,32 @@ def combine_histograms(hists):
 
 
 def combine_ehist(path, runs):
+    """Combine a set of energy visited states histograms.
+
+    Args:
+        path: The base path containing the data to combine.
+        runs: The list of runs to combine.
+
+    Returns:
+        A combined energy histogram.
+    """
     hists = read_histograms_from_runs(path, runs, 'ehist.dat')
 
     return combine_histograms(hists)
 
 
 def combine_all_nhists(path, runs):
+    """Combine all molecule number visited states histograms from a
+    set of runs.
+
+    Args:
+       path: The base path containing the data to combine.
+       runs: The list of runs to combine.
+
+    Returns:
+        A list of combined histograms, with one entry for each
+        species.
+    """
     hist_files = [os.path.basename(f)
                   for f in glob.glob(os.path.join(runs[0],'nhist_*.dat'))]
 
@@ -53,12 +73,33 @@ def combine_all_nhists(path, runs):
 
 
 def combine_vhist(path, runs, uses_log_volume=False):
+    """Combine a set of energy visited states histograms.
+
+    Args:
+        path: The base path containing the data to combine.
+        runs: The list of runs to combine.
+        uses_log_volume: A bool denoting whether the bins in the
+            histogram use V or ln(V).
+
+    Returns:
+        A combined volume histogram.
+    """
     hists = read_volume_histograms_from_runs(path, runs, uses_log_volume)
 
     return combine_histograms(hists)
 
 
 def combine_hits_op(path, runs):
+    """Combine a set of order parameter hits files.
+
+    Args:
+        path: The base path containing the data to combine.
+        runs: The list of runs to combine.
+
+    Returns:
+        A tuple containing the order parameter values and the combined
+        hits for each value.
+    """
     index = np.loadtxt(os.path.join(path, runs[0], 'hits_op.dat'),
                        usecols=(0, ))
 
@@ -67,6 +108,17 @@ def combine_hits_op(path, runs):
 
 
 def combine_hits_tr(path, runs):
+    """Combine a set of growth expanded ensemble hits files.
+
+    Args:
+        path: The base path containing the data to combine.
+        runs: The list of runs to combine.
+
+    Returns:
+        A tuple containing the first five columns of the combined
+        hits_tr.dat file, i.e., the index, order parameter value,
+        species ID, growth stage, and combined number of hits.
+    """
     index, sub, mol, stage = np.loadtxt(os.path.join(path, runs[0],
                                                      'hits_tr.dat'),
                                         usecols=(0, 1, 2, 3))
@@ -77,6 +129,18 @@ def combine_hits_tr(path, runs):
 
 
 def combine_prop(path, runs, file_name):
+    """Combine a set of property files.
+
+    Args:
+        path: The base path containing the data to combine.
+        runs: The list of runs to combine.
+        file_name: The file to combine: 'prop_op.dat', 'prop_tr.dat',
+            or 'prop_ex.dat'.
+
+    Returns:
+        A tuple containing the index column and a numpy array with the
+        remaining columns of the combined property files.
+    """
 
     def read_properties(run):
         return np.transpose(np.loadtxt(os.path.join(path, run, file_name)))[1:]
@@ -103,6 +167,16 @@ def combine_prop(path, runs, file_name):
 
 
 def combine_pzcnt(path, runs):
+    """Combine a set of density histogram count files.
+
+    Args:
+        path: The base path containing the data to combine.
+        runs: The list of runs to combine.
+
+    Returns:
+        A tuple containing the index and combined density histogram
+        counts for each order parameter value.
+    """
     index = np.loadtxt(os.path.join(path, runs[0], 'pzcnt.dat'),
                        usecols=(0, ))
 
@@ -111,6 +185,17 @@ def combine_pzcnt(path, runs):
 
 
 def combine_all_pzhists(path, runs):
+    """Combine all denisty histograms for a set of runs.
+
+    Args:
+        path: The base path containing the data to combine.
+        runs: The list of runs to combine.
+
+    Returns:
+        A tuple containing the density histogram index, z distance
+        bins, and a dict with the combined histograms, indexed by
+        file name.
+    """
     index, z_bins = np.loadtxt(os.path.join(path, runs[0], 'pzhist_01_01.dat'),
                                usecols=(0, 1), unpack=True)
     cnts = [np.loadtxt(os.path.join(path, r, 'pzcnt.dat'), usecols=(1, ))]
