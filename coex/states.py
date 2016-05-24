17e7e7d3a3db6f50557077ba42f94c049a0ee4b4
Adam Rall <rall@openmailbox.org>
2016-05-24 00:44:53 -0400

    Use classmethods instead of read_* functions.

--- coex/states.py
+++ coex/states.py
@@ -1,4 +1,5 @@
-"""Functions for working with visited states distributions.
+"""Objects and functions for working with visited states
+distributions.
 
 Visited states distributions are used to find the change in free
 energy via histogram reweighting. For example, an energy visited
@@ -85,6 +86,18 @@ class VisitedStatesDistribution(object):
 
         return sum(self.bins * shifted) / sum(shifted)
 
+    @staticmethod
+    def from_file(path, subensemble):
+        if 'vhist' in os.path.basename(path):
+            return VolumeVisitedStatesHistogram.from_file(path)[subensemble]
+
+        return VisitedStatesHistogram.from_file(path)[subensemble]
+
+
+def _get_limits_path(hist_file):
+    return os.path.join(os.path.dirname(hist_file),
+                        os.path.basename(hist_file).replace('hist', 'lim'))
+
 
 class VisitedStatesHistogram(object):
     def __init__(self, distributions):
@@ -116,6 +129,40 @@ class VisitedStatesHistogram(object):
 
         return np.array([d.average(weights[i]) for i, d in self])
 
+    @classmethod
+    def from_file(cls, path):
+        """Read a visited states histogram from a pair of files.
+
+        This method accepts the location of the raw histogram file, e.g.,
+        ehist.dat and parses the appropriate limits file (here, elim.dat)
+        in the same directory.
+
+        Args:
+            path: The location of the raw histogram data.
+
+        Returns:
+            A VisitedStatesHistogram object.
+        """
+        raw_hist = np.transpose(np.loadtxt(path))[1:]
+        limits = np.loadtxt(_get_limits_path(path))
+
+        def create_distribution(line):
+            sub, size, lower, upper, step = line
+            sub, size = int(sub), int(size)
+            bins = np.linspace(lower, upper, size)
+            if 'nhist' in path:
+                bins = bins.astype('int')
+
+            if len(raw_hist.shape) == 1:
+                counts = np.array([raw_hist[sub]])
+            else:
+                counts = raw_hist[sub][0:size]
+
+            return VisitedStatesDistribution(bins=bins,
+                                             counts=counts.astype('int'))
+
+        return cls([create_distribution(line) for line in limits])
+
     def write(self, path):
         most_sampled = 0
         with open(_get_limits_path(path), 'w') as f:
@@ -137,26 +184,52 @@ class VisitedStatesHistogram(object):
         np.savetxt(path, raw_hist, fmt='%8.d', delimiter='  ')
 
 
+def read_all_molecule_histograms(path):
+    """Read all of the molecule number visited states histograms in a
+    directory.
+
+    Args:
+        path: The directory containing the the nhist and nlim files
+            to read.
+
+    Returns:
+        A list of histograms as read by read_histogram. Each one is
+        itself a list of dicts with the keys 'bins' and 'counts'.
+
+    See Also:
+        read_histogram()
+    """
+    hist_files = sorted(glob.glob(os.path.join(path, "nhist_??.dat")))
+
+    return [VisitedStatesHistogram.from_file(f) for f in hist_files]
+
+
 class VolumeVisitedStatesHistogram(VisitedStatesHistogram):
     def __init__(self, distributions, use_log_volume=False):
         self.use_log_volume = use_log_volume
-        super().__init__(distributions)
-
+        super(VolumeVisitedStatesHistogram, self).__init__(distributions)
         for d in self:
             if use_log_volume:
                 d.bins = np.exp(d.bins)
 
             d.bins *= cubic_meters
 
+    @classmethod
+    def from_file(cls, path, use_log_volume=False):
+        hist = super(VisitedStatesHistogram, cls).from_file(path)
+        hist.use_log_volume = use_log_volume
+
+        return hist
+
     def write(self, path):
-        copy_with_units = copy.copy(self)
-        for d in copy_with_units:
-            if d.use_log_volume:
+        units_copy = copy.copy(self)
+        for d in units_copy:
+            if units_copy.use_log_volume:
                 d.bins = np.log(d.bins)
 
             d.bins /= cubic_meters
 
-        super(VisitedStatesHistogram, copy_with_units).write(path)
+        super(VisitedStatesHistogram, units_copy).write(path)
 
 
 def combine_histograms(hists):
@@ -192,103 +265,53 @@ def combine_histograms(hists):
     return VisitedStatesHistogram(distributions)
 
 
-def read_all_molecule_histograms(path):
-    """Read all of the molecule number visited states histograms in a
-    directory.
+def combine_energy_histograms(path, runs):
+    """Combine a set of energy visited states histograms.
 
     Args:
-        path: The directory containing the the nhist and nlim files
-            to read.
+        path: The base path containing the data to combine.
+        runs: The list of runs to combine.
 
     Returns:
-        A list of histograms as read by read_histogram. Each one is
-        itself a list of dicts with the keys 'bins' and 'counts'.
-
-    See Also:
-        read_histogram()
+        A combined energy histogram.
     """
-    hist_files = sorted(glob.glob(os.path.join(path, "nhist_??.dat")))
+    return combine_histograms([VisitedStatesHistogram.from_file(
+        os.path.join(path, r, 'ehist.dat')) for r in runs])
 
-    return [read_histogram(hist) for hist in hist_files]
 
-
-def read_energy_distribution(path, subensemble):
-    """Read an energy visited states distribution.
-
-    This function reads the energy visited states histogram from a
-    specified directory but returns only the distribution for a
-    specified subensemble.  It is used for reweighting a subensemble
-    in a TEE simulation to a reference point of different beta.
+def combine_all_molecule_histograms(path, runs):
+    """Combine all molecule number visited states histograms from a
+    set of runs.
 
     Args:
-        path: The directory containing the ehist and elim files to
-            read.
-        subensemble: The specific distribution requested.
+       path: The base path containing the data to combine.
+       runs: The list of runs to combine.
 
     Returns:
-        A distribution, i.e., a dict with the keys 'bins' and
-        'counts'.
+        A list of combined histograms, with one entry for each
+        species.
     """
-    return read_histogram(os.path.join(path, 'ehist.dat'))[subensemble]
+    hist_files = [os.path.basename(f)
+                  for f in glob.glob(os.path.join(runs[0], 'nhist_*.dat'))]
+
+    def read_hists(hist_file):
+        return [VisitedStatesHistogram.from_file(
+            os.path.join(path, r, hist_file)) for r in runs]
+
+    return [combine_histograms(read_hists(hf)) for hf in hist_files]
 
 
-def _get_limits_path(hist_file):
-    return os.path.join(os.path.dirname(hist_file),
-                        os.path.basename(hist_file).replace('hist', 'lim'))
-
-
-def read_histogram(path):
-    """Read a visited states histogram from a pair of files.
+def combine_volume_histograms(path, runs, use_log_volume=False):
+    """Combine a set of energy visited states histograms.
 
     Args:
-        path: The location of the raw histogram data.
+        path: The base path containing the data to combine.
+        runs: The list of runs to combine.
+        use_log_volume: A bool denoting whether the bins in the
+            histogram use V or ln(V).
 
     Returns:
-        A histogram, i.e., a list of distributions.
+        A combined volume histogram.
     """
-    raw_hist = np.transpose(np.loadtxt(path))[1:]
-    limits = np.loadtxt(_get_limits_path(path))
-
-    def create_distribution(line):
-        sub, size, lower, upper, step = line
-        sub, size = int(sub), int(size)
-        bins = np.linspace(lower, upper, size)
-        if 'nhist' in path:
-            bins = bins.astype('int')
-
-        if len(raw_hist.shape) == 1:
-            counts = np.array([raw_hist[sub]])
-        else:
-            counts = raw_hist[sub][0:size]
-
-        return VisitedStatesDistribution(bins=bins,
-                                         counts=counts.astype('int'))
-
-    return VisitedStatesHistogram([create_distribution(line)
-                                   for line in limits])
-
-
-def read_histograms_from_runs(path, runs, hist_name):
-    return [read_histogram(os.path.join(path, r, hist_name))
-            for r in sorted(runs)]
-
-
-def read_volume_histogram(path, use_log_volume=False):
-    """Read a volume visited states histogram from a pair of files.
-
-    Args:
-        path: The location of the raw histogram data.
-        use_log_volume: A bool denoting whether the histogram bins
-            store the volume or the logarithm of the volume.
-
-    Returns:
-        A histogram, i.e., a list of distributions.
-    """
-    return VolumeVisitedStatesHistogram(read_histogram(path).distributions,
-                                        use_log_volume)
-
-
-def read_volume_histograms_from_runs(path, runs, use_log_volume=False):
-    return [read_volume_histogram(os.path.join(path, r, 'vhist.dat'),
-                                  use_log_volume)
-            for r in sorted(runs)]
+    return combine_histograms([VolumeVisitedStatesHistogram.from_file(
+        os.path.join(path, r, 'vhist.dat'), use_log_volume) for r in runs])
