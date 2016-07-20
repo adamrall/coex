Fix average_n for single-compoent systems.

coex/gc.py
@@ -100,15 +100,21 @@ class Simulation(object):
         """
         bound = int(split * len(self.dist))
 
-        def average_phase(hist, weight, phase):
-            split_hist = hist[:bound] if phase == 'vapor' else hist[bound:]
+        def average_phase(hist, weight, is_vapor=True):
+            split_hist = hist[:bound] if is_vapor else hist[bound:]
 
             return [d.average(weight) for d in split_hist]
 
-        vapor = np.array([average_phase(nh, self.weights[i], 'vapor')
-                          for i, nh in enumerate(self.nhists[1:])])
-        liquid = np.array([average_phase(nh, self.weights[i], 'liquid')
-                           for i, nh in enumerate(self.nhists[1:])])
+        hists = self.nhists[1:]
+        if len(hists) > 1:
+            vapor = np.array([average_phase(nh, self.weights[i])
+                              for i, nh in enumerate(hists)])
+            liquid = np.array(
+                [average_phase(nh, self.weights[i], is_vapor=False)
+                 for i, nh in enumerate(hists)])
+        else:
+            vapor = average_phase(hists[0], self.weights[0])
+            liquid = average_phase(hists[0], self.weights[0], is_vapor=False)
 
         return {'vapor': vapor, 'liquid': liquid}
 

coex/gcee.py
@@ -44,7 +44,7 @@ class Phase(object):
         self.beta = beta
         self.weights = weights
         if weights is None:
-            self.weights = np.tile(None, len(dist))
+            self.weights = np.tile(None, activities.shape)
 
     @property
     def fractions(self):
@@ -62,6 +62,7 @@ class Phase(object):
             solutions: A list of log(activitiy) differences.
             species: The species used in histogram reweighting.
         """
+        log_old_act = np.log(self.activities)
         if species == 0:
             frac = activities_to_fractions(self.activities)
             frac[0] -= solutions
@@ -70,10 +71,10 @@ class Phase(object):
         for i, sol in enumerate(solutions):
             self.dist.log_probs[i] += self.nhists[species][i].reweight(sol)
             if species != 0:
-                log_old_act = np.log(self.activities[species - 1, i])
-                log_new_act = log_old_act - sol
-                self.weights[i] = np.nan_to_num(log_old_act - log_new_act)
-                self.activities[species - 1, i] = np.exp(log_new_act)
+                new_act = np.exp(log_old_act[species - 1, i] - sol)
+                self.activities[species - 1, i] = new_act
+
+        self.weights = np.nan_to_num(log_old_act - np.log(self.activities))
 
     @property
     def composition(self):
@@ -136,8 +137,12 @@ class Phase(object):
             A numpy array with the number of molecules of each species
             in each subensemble.
         """
-        return np.array([h.average(w)
-                         for h, w in zip(self.nhists[1:], self.weights)])
+        hists = self.nhists[1:]
+        if len(hists > 1):
+            return np.array([h.average(w)
+                             for h, w in zip(hists, self.weights)])
+
+        return hists[0].average(self.weights[0])
 
 
 def read_phase(path, index, fractions=None, beta=None):